4c56a7860df81640407aad71cc31c75f70860156
Amiel Terry <147953432+amielterry26@users.noreply.github.com>
2024-10-26 00:28:09 -0700

    Added biweekly to code

--- finished_projects/salary.py
+++ finished_projects/salary.py
@@ -1,11 +1,13 @@
 salary = int(input("What is your yearly salary? "))
 month = salary//12
+biweekly = month//2
 week = month//4
 day = week//5
 hour = day//8
 
 print(f"Your salary is ${salary}")
 print(f"You make ${month} per month.")
+print(f"You make ${biweekly} per month.")
 print(f"You make ${week} per week.")
 print(f"You make ${day} per day.")
 print(f"You make ${hour} per hour.")
