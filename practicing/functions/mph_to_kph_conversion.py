08538294a7f04355b679e3ab23d28e6f45b8dba3
Amiel Terry <147953432+amielterry26@users.noreply.github.com>
2024-11-10 17:33:38 -0800

    added multiple function files

--- practicing/functions/mph_to_kph_conversion.py
+++ practicing/functions/mph_to_kph_conversion.py
@@ -8,14 +8,24 @@ def mph(speed):
     print(f"You were going {result} mph.")
 
 
-speed = int(input("How fast were you going? "))
-ph = input("mph or kph? ")
-error = "please input mph or kph, and run script again."
+def main():
+    while True:
+        speed = int(input("How fast were you going? "))
+        ph = input("mph or kph? ")
+        error = "please input mph or kph, and run script again."
 
-if ph == "mph":
-    kph(speed)
-elif ph == "kph":
-    mph(speed)
-else:
-    print(error)
+        if ph == "mph":
+            kph(speed)
+        elif ph == "kph":
+            mph(speed)
+        else:
+            print(error)
 
+        answer = input("Would you like to make another calculation?")
+        if answer == "y":
+            main()
+        else:
+            break
+
+
+main()
