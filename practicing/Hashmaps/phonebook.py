751c139aa7144c26b7fd2ecd04ee7d87d7592c26
Amiel Terry <147953432+amielterry26@users.noreply.github.com>
2024-11-12 11:55:23 -0800

    added Hashmaps

---
+++ practicing/Hashmaps/phonebook.py
@@ -0,0 +1,9 @@
+# This is an example of a simple phonebook dictionary using a hashmap
+phone_book = {
+    "Amiel": "555-1234",
+    "Jo": "555-3456",
+    "Bernard": "555-9876"
+}
+
+# To access a value in hashmap using keys
+print(phone_book["Jo"])
